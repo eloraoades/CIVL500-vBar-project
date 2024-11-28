64967cc4334f019f00e3a8e552994abc6593a86a
Elora M Oades <39773782+eloraoades@users.noreply.github.com>
2024-11-28 10:40:30 -0500

    Survey Data

--- plotSurveyData.m
+++ plotSurveyData.m
@@ -1,8 +1,5 @@
-filePath = cd; 
-q = genpath(filePath); addpath(q);
 
-%% Load data & define grids
-% You can change this fieldName to be any of the surveys (should all be in
+% You can change htis fieldName to be any of the surveys (should all be in
 % the same format)
 fileName = "FRF_20190909_1169_FRF_NAVD88_LARC_GPS_UTC_v20190916_grid_latlon.txt";
 
@@ -30,7 +27,7 @@ Y = yMin:delY:yMax;
 % Project depth data onto the regularly spaced grid we just created
 zq = griddata(surveyData.X, surveyData.Y, surveyData.Z, xq, yq);
 
-%% Now plot data :) 
+%% Now plot it :) 
 
 % what y-location are we looking at for OCM? 
 yOCM = 650;
@@ -43,14 +40,13 @@ title(sprintf('%s', surveyDatetime));
 c = colorbar(); c.Label.String = 'h (m)';
 set(gca, 'FontName', 'Times', 'FontSize', 18, 'box', 'on', 'Layer', 'top');
 clim([-10 4]);
-cmocean('topo', 'pivot', 0);
 
 % Add the pier 
 hold on; 
 pier = line([50 500], [515 515], 'LineWidth', 1.2, 'Color', 'k');
 
 % and the sensors
-awac45m = scatter(400, 940, 'square', 'filled', 'MarkerEdgeColor','k', 'MarkerFaceColor', [1.00,1.00,0.00], 'SizeData',50);
+awac45m = scatter(400, 940, 'square', 'filled', 'MarkerEdgeColor','k', 'MarkerFaceColor', [0.72,0.27,1.00], 'SizeData',50);
 
 % ocm 
 ocm1 = scatter(125, yOCM, 'o', 'filled', 'MarkerEdgeColor', 'k', 'MarkerFaceColor','w');
